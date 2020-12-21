1162f7f6c2c6097773619acf8e293e7c26bcbf01
MinaSaad47 <MinaSaad-47@outlook.com>
2020-12-21 22:07:03 +0200

    Added an icon

--- src/electronic_library_gui_java/Electronic_Library_GUI_JAVA.java
+++ src/electronic_library_gui_java/Electronic_Library_GUI_JAVA.java
@@ -1,11 +1,20 @@
 
 package electronic_library_gui_java;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
 
 public class Electronic_Library_GUI_JAVA {
 
     
     public static void main(String[] args) {
+        JFrame window = new JFrame("Electronic Library");
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
+        ImageIcon icon = new ImageIcon("img/icon.png");
+        window.setIconImage(icon.getImage());
         
     }
     
