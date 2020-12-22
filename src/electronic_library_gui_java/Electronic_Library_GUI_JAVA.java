a0c7d4ac8440836b2c98b0e10b0375fb2f11fb8c
MinaSaad47 <MinaSaad-47@outlook.com>
2020-12-22 20:18:23 +0200

    test

--- src/electronic_library_gui_java/Electronic_Library_GUI_JAVA.java
+++ src/electronic_library_gui_java/Electronic_Library_GUI_JAVA.java
@@ -2,6 +2,7 @@
 package electronic_library_gui_java;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
@@ -13,8 +14,13 @@ public class Electronic_Library_GUI_JAVA {
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(800, 600);
+        window.setLayout(null);
         ImageIcon icon = new ImageIcon("img/icon.png");
         window.setIconImage(icon.getImage());
+        JButton button = new JButton("Click Me!");
+        button.setSize(100, 200);
+        button.setVisible(true);
+        window.add(button, JButton.CENTER);
         
     }
     
